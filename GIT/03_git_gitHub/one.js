cebda09b27c39cd3e5815e5dadcb55c3605f733b
Rishav chopra <choprarishav9@gmail.com>
2025-09-03 22:52:40 +0530

    FIRST COMMIT

--- GIT/03_git_gitHub/one.js
+++ GIT/03_git_gitHub/one.js
@@ -6,3 +6,20 @@ console.log(loginpage)
 
 const fotter ="footer added into the website"
 console.log(fotter);
+
+
+
+// PS C:\Users\acer\Desktop\PROJECTS\RECE> git branch
+// * feature
+//   master
+// PS C:\Users\acer\Desktop\PROJECTS\RECE> git checkout master
+// M       REACT/bundlers
+// Switched to branch 'master'
+// PS C:\Users\acer\Desktop\PROJECTS\RECE> git merge feature
+// Updating 2134507..984fc36
+// Fast-forward
+//  GIT/git_02/one.js | 17 ++++++++++++++++-
+//  1 file changed, 16 insertions(+), 1 deletion(-)
+// PS C:\Users\acer\Desktop\PROJECTS\RECE> git branch -D feature
+// Deleted branch feature (was 984fc36).
+// PS C:\Users\acer\Desktop\PROJECTS\RECE>
